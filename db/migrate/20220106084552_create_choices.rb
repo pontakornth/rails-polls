d75dd32d72dc38b7d060318395918a4284cad5e4
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-01-06 15:49:55 +0700

    feat: Add choice model

--- db/migrate/20220106084552_create_choices.rb
+++ db/migrate/20220106084552_create_choices.rb
@@ -2,7 +2,7 @@ class CreateChoices < ActiveRecord::Migration[6.1]
   def change
     create_table :choices do |t|
       t.string :choice_text
-      t.number :votes
+      t.integer :votes
 
       t.timestamps
     end
