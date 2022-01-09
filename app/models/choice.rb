354209ead1961d7cd1281a9318abf10a8d1af06c
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-01-09 14:40:27 +0700

    feat: add validation to both models

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,6 @@
 class Choice < ApplicationRecord
   belongs_to :question
+  validates :choice_text, presence: true
 
   has_many :votes
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :choices
+  validates :question_text, :end_date, presence: true
   def can_vote
     end_date - Time.now >= 0
   end
